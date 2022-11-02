a82823ad30d8ff82f21b8ee1225f4324e87f0ad5
Caian Ertl <caian.ertl@mzgroup.com>
2022-11-02 17:43:46 -0300

    chore: import the old site code base

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: ['./tsconfig.json']
+      }
+    }
+  ],
+  extends: ['standard-with-typescript']
+}
